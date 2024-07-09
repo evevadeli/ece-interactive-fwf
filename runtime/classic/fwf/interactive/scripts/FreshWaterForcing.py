3b6e6014a99441cccaab83e23df3561b073ae6f9
Eveline van der Linden <linden@knmi.nl>
2024-07-09 14:13:50 +0000

    Solved error in freshwater forcing computation

--- runtime/classic/fwf/interactive/scripts/FreshWaterForcing.py
+++ runtime/classic/fwf/interactive/scripts/FreshWaterForcing.py
@@ -40,19 +40,19 @@ def freshwater_flux_anomaly_df(t, length, BM, RF, file_future_fwf):
     # length of RF is 200 years (0:199) - only need years up to year_max
     lenRF = len(RF)
     # If there are less then 200 years left:
-    if (length - t) < lenRF:
-        #1902:2100
-        dfFWF.iloc[t:length] = dfFWF.iloc[t:length].values + dFWFyear.iloc[:(length-t)].values
+    #if (length - t) < lenRF:
+    #    #1902:2100
+    #    dfFWF.iloc[t:length] = dfFWF.iloc[t:length].values + dFWFyear.iloc[:(length-t)].values
     #If there are more than 200 years left:
-    elif (length - t) >= lenRF:
-        #1850:1901
-        dfFWF.iloc[t:t+lenRF] = dfFWF.iloc[t:t+lenRF].values + dFWFyear.iloc[:].values
+    #elif (length - t) >= lenRF:
+    #    #1850:1901
+    dfFWF.iloc[t:t+lenRF] = dfFWF.iloc[t:t+lenRF].values + dFWFyear.iloc[:].values
     # Write dfFWF to file (updated each year) !! the years from year_min to year should have been processed before
     dfFWF.to_csv(file_future_fwf)
     print(f'Saved future forcing to {file_future_fwf}')
     # Compute differences: annual forcing
     dfFWF_diff = dfFWF.diff()
     print('dfFWF_diff')
-    print(pd.DataFrame(dfFWF_diff.iloc[t+1]).T)
+    print(pd.DataFrame(dfFWF_diff.iloc[t]).T)
 
-    return (pd.DataFrame(dfFWF_diff.iloc[t+1]).T)
+    return (pd.DataFrame(dfFWF_diff.iloc[t]).T)
